645928656bfdec97c3c14331adf5b0ceccd092cc
Kai Chen <kaichen@outlook.com>
2019-09-04 17:32:08 -0700

    Add image augmentation

--- catsordogs/catsordogs.py
+++ catsordogs/catsordogs.py
@@ -90,14 +90,28 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
 
 TRAINING_DIR = "/tmp/cats-v-dogs/training/"
-train_datagen = ImageDataGenerator(rescale=1.0/255.)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
 train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
                                                     batch_size=100,
                                                     class_mode='binary',
                                                     target_size=(150, 150))
 
 VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
-validation_datagen = ImageDataGenerator(rescale=1.0/255.)
+validation_datagen = ImageDataGenerator(rescale=1./255,
+                                        rotation_range=40,
+                                        width_shift_range=0.2,
+                                        height_shift_range=0.2,
+                                        shear_range=0.2,
+                                        zoom_range=0.2,
+                                        horizontal_flip=True,
+                                        fill_mode='nearest')
 validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
                                                               batch_size=100,
                                                               class_mode='binary',
