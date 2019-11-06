963e7c13521cbea7aca7ff45e5860682cd1ad6db
Kai Chen <kaichen@outlook.com>
2019-11-05 16:59:00 -0800

    Classify horses vs humans using Transfer Learning with Inception

---
+++ horsesorhumans/horsesorhumans.py
@@ -0,0 +1,103 @@
+import os
+import zipfile
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+!wget --no-check-certificate \
+    https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5 \
+    -O /tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5
+
+!wget --no-check-certificate https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip -O /tmp/horse-or-human.zip
+
+!wget --no-check-certificate https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip -O /tmp/validation-horse-or-human.zip
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('acc') > 0.999):
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+local_weights_file = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
+                                include_top=False,
+                                weights=None)
+
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+pre_trained_model.summary()
+
+last_layer = pre_trained_model.get_layer('mixed7')
+last_output = last_layer.output
+
+x = layers.Flatten()(last_output)
+x = layers.Dense(1024, activation='relu')(x)
+x = layers.Dropout(0.2)(x)
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+
+model.compile(optimizer=RMSprop(lr=0.0001),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+model.summary()
+
+local_zip = '//tmp/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/training')
+zip_ref.close()
+
+local_zip = '//tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation')
+zip_ref.close()
+
+train_dir = '/tmp/training'
+validation_dir = '/tmp/validation'
+
+train_horses_dir = os.path.join(train_dir, 'horses')
+train_humans_dir = os.path.join(train_dir, 'humans')
+validation_horses_dir = os.path.join(validation_dir, 'horses')
+validation_humans_dir = os.path.join(validation_dir, 'humans')
+
+train_horses_fnames = os.listdir(train_horses_dir)
+train_humans_fnames = os.listdir(train_humans_dir)
+validation_horses_fnames = os.listdir(validation_horses_dir)
+validation_humans_fnames = os.listdir(validation_humans_dir)
+
+train_datagen = ImageDataGenerator(rescale=1./255.,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1.0/255.)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                        batch_size=20,
+                                                        class_mode='binary',
+                                                        target_size=(150, 150))
+
+callbacks = myCallback()
+history = model.fit_generator(
+            train_generator,
+            validation_data=validation_generator,
+            steps_per_epoch=100,
+            epochs=100,
+            validation_steps=50,
+            verbose=2,
+            callbacks=[callbacks])
